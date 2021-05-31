2a383e0c096fafcf1edb2eec39bb7b17ba59a092
basac.scg <basac.scg@gmail.com>
2021-05-30 21:58:34 -0600

    Se agrega practicas de excepciones, practicas escritura y lectura de archivos en python

---
+++ intermediate_python/hangman.py
@@ -0,0 +1,18 @@
+
+
+
+# Reglas
+# Usar comprehensions, manjeo de errores y manjeo de archivos
+# utilizar el archivo data.txt y leelo para obetner las palabras
+
+#Usar funcion enumarate
+#El metodo get de los diccionarios puee servirte
+#La sentencia 
+#       os.system('cls') -> Windows
+#        os.system('clear') -> unix
+# servira para limpiar la pantalla
+
+# Mejoras
+# Añade un sistema de puntos
+# Dibuja al ahoracado en cada jugada con codigo ASCII
+# Mejora la interfaz
